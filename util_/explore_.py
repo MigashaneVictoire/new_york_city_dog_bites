ea537e6cd97a8603775c82e975ff23629f2965e7
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-08-02 15:51:28 -0500

    add model.py and explore.py

---
+++ util_/explore_.py
@@ -0,0 +1,118 @@
+# data manipulation
+import pandas as pd
+import numpy as np
+
+# data visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# seasonal decomposition
+import statsmodels.api as sm
+
+# system manipulation
+import itertools
+import os
+import sys
+sys.path.append("./util_")
+import prepare_
+# import explore_
+
+# other
+import warnings
+warnings.filterwarnings("ignore")
+
+# set the random seed
+np.random.seed(95)
+
+
+# set a default them for all my visuals
+sns.set_theme(style="whitegrid")
+
+# _______________________ Set up___________________________________
+# load data from existing csv
+dogs_train = pd.read_csv("./00_project_data/1-1_training_data.csv", index_col=0)
+
+# make sure the index in in time format
+dogs_train.index = dogs_train.index.astype("datetime64")
+
+train = dogs_train.resample('D').bite.sum()
+train = pd.DataFrame(train)
+
+# create a year, month, day column from the index
+train["year"] = train.index.year
+train["month"] = train.index.month_name()
+train["week day"] = train.index.day_name()
+train["day of month"] = train.index.day
+train["month number"] = train.index.month
+train["week day number"] = train.index.weekday
+
+# _____________________Explore Functions____________________________________
+
+def get_trend1():
+    """
+    Return plot for answering the following quesion:
+        - Are there any noticeable trends in the number of dog bites over time?
+    """
+    
+    plt.figure(figsize=(8,4))
+    train.bite.resample('D').mean().plot(alpha=.5, label='Daily')
+    train.bite.resample('W').mean().plot(alpha=.8, label='Weekly')
+    train.bite.resample('M').mean().plot(label='Montly')
+    train.bite.resample('Y').mean().plot(label='Yearly')
+    plt.title("General dog bite trend over time")
+    plt.xlabel('date of bite')
+    plt.ylabel('Frequency')
+    plt.legend()
+    return plt.gcf()
+
+def get_high_month():
+    """
+    Return plot for answering the following quesion:
+        - Are certain months or seasons associated with a higher number of incidents?
+    """
+    # name of month to choose from
+    x_labes = x_labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    colors = ['blue', 'blue', 'blue', 'orange', 'red', 'red', 'red', 'red', 'orange', 'orange', 'orange', 'blue']
+
+   # Yearly mean bits
+    plt.figure(figsize=(7,3))
+    train.groupby("month number").bite.mean().plot(kind="bar", color=colors)
+    plt.title("Monthly mean bite")
+    plt.xticks(range(12), x_labels)
+    plt.xlabel("month")
+    plt.ylabel("bite")
+    plt.xticks(rotation=0)
+    return plt.gcf()
+
+def get_week_diff():
+    """
+    Return plot for answering the following quesion:
+        - Are there specific days of the week when dog bites are more frequent than others?
+    """
+    # name of month to choose from
+    x_labels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    colors = ['orange', 'orange', 'orange', 'orange', 'orange', 'red', 'red']
+    # Yearly mean bits
+    plt.figure(figsize=(7,3))
+    train.groupby("week day number").bite.mean().plot(kind="bar", color=colors)
+    plt.title("Weekday mean bites")
+    plt.xticks(range(7), x_labels)
+    plt.xlabel("week day")
+    plt.ylabel("bite")
+    plt.xticks(rotation=0)
+    return plt.gcf()
+
+def get_borough():
+    """
+    Return plot for answering the following quesion:
+        - What impact does dog bite have in differnt locations?
+    """
+    colors = ['red', 'red', 'red', 'orange', 'orange', 'orange']
+    # borough of bite
+    plt.figure(figsize=(7, 3))
+    dogs_train.borough.value_counts().plot.bar(color=colors)
+    plt.title("bite count by borough")
+    plt.xlabel("week day")
+    plt.ylabel("total bites")
+    plt.xticks(rotation=0)
+    return plt.gcf()
---
+++ util_/model_.py
@@ -0,0 +1,114 @@
+# for presentation purposes
+import warnings
+warnings.filterwarnings("ignore")
+
+# transform
+import numpy as np
+import pandas as pd
+
+# visualize 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# working with dates
+from datetime import datetime
+
+# modeling
+import statsmodels.api as sm
+from statsmodels.tsa.api import Holt, ExponentialSmoothing
+
+# evaluate
+from sklearn.metrics import mean_squared_error
+from math import sqrt 
+
+np.random.seed(95)
+import math
+
+# load data from existing csv
+train = pd.read_csv("./00_project_data/1-1_training_data.csv", index_col=0)
+validate = pd.read_csv("./00_project_data/1-2_validation_data.csv", index_col=0)
+test = pd.read_csv("./00_project_data/1-3_testing_data.csv", index_col=0)
+
+# covert the index to datetime
+train.index = train.index.astype("datetime64")
+validate.index = validate.index.astype("datetime64")
+test.index = test.index.astype("datetime64")
+
+# resample to only weekly data
+train = train.resample('w').bite.sum()
+validate = validate.resample('w').bite.sum()
+test = test.resample('w').bite.sum()
+train = pd.DataFrame(train)
+validate = pd.DataFrame(validate)
+test = pd.DataFrame(test)
+
+# create evaluation dataframe
+eval_df = pd.DataFrame(columns=["model", "rmse"])
+
+# evaluation funtion
+def evaluate(target):
+    """
+    Takes in the target col name and return the root mean square error for
+    the validate/test against the predicted values.
+    """
+    rmse = round(sqrt( # get root mean square error
+        mean_squared_error(validate[target], y_pred_df[target])),3) # get mean squared error
+    return rmse
+    
+# plot predictions vs actual values
+def plot_evaluate(target):
+    """
+    Function will return a plot of the predicted values against the actual values in the data.
+    """
+    # plt.ioff()
+    # plt the value value fof bite over time and the predicted
+    plt.figure(figsize=(8,3))
+    plt.plot(train.bite, label="Train", linewidth=1)
+    plt.plot(validate.bite,label="Validate", linewidth=1)
+    plt.plot(y_pred_df[target],label="Prediction", linewidth=1 )
+    plt.title("Dog bite over time prediction")
+    plt.legend()
+
+# function to append to the evaluation dataframe
+def append_to_eval(model, target):
+    """
+    Function append the current running model results the historical models.
+    """
+    rmse = evaluate(target)
+    res_dict = pd.DataFrame({"model":[model],
+               "rmse":[rmse]})
+    return pd.concat([eval_df, res_dict])
+
+def final_results_non_auto():
+    """
+    Goal: return the best rmse for each of the models
+    """
+    md = pd.DataFrame({"Model": ["Moving average 23wk", "Holt linear trend", 
+                        "Holt seasonal trend", "Previous cycle"],
+              "RMSE":[16.449, 38.744, 0, 21.760]})
+
+    colors = ["green", "red", "orange", "orange"]
+    plt.figure(figsize=(8,3))
+    sns.barplot(data= md, x="Model", y='RMSE', palette=colors)
+    plt.title("Model RMSE weekly")
+    return plt.gcf()
+
+def get_best_model():
+    """
+    Goal: return the best visual
+    """
+    md = pd.DataFrame({"Model": ["Moving average 23wk", "Holt linear trend", 
+                        "Holt seasonal trend", "Previous cycle"],
+              "RMSE":[16.449, 38.744, 0, 21.760]})
+
+    colors = ["blue", "gray", "gray", "gray"]
+    plt.figure(figsize=(8,3))
+    sns.barplot(data= md, x="Model", y='RMSE', palette=colors)
+    plt.title("Model RMSE weekly")
+
+    # Annotate each bar with its corresponding value
+    for index, value in enumerate(md['RMSE']):
+        ax.text(index, value, str(value), ha='center', va='bottom'
+    return plt.gcf()
+
+
